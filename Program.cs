17c824deaf7b897fc46e58d3c758ce2bb9bb3ac5
bsladania <bikramjeetsingh@live.com>
2021-08-12 23:58:39 -0400

    Project setup

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Credits: 3BUXO4P24no
+
+using Microsoft.Azure.Management.Compute.Fluent;
+using Microsoft.Azure.Management.Compute.Fluent.Models;
+using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+
+namespace AzureVMDeployment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Windows Virtual Machine Devlopment has been started...");
+
+            //Get Azure credentials
+            
+        }
+    }
+}
